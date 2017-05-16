1f74e5a8b7d401ab0c186b913f502bf524f9569d
syaning <syaningv@gmail.com>
2017-05-16 18:20:15 +0800

    update build

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ cd _book
 git init
 git add -A
 git commit -m "update docs"
-git push -f git@github.com:syaning/go-pkgs.git master:gh-pages
+git push -f https://github.com/syaning/go-pkgs.git master:gh-pages
